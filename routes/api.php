661b6fd735071f0151ca33ad0f075ffed5f8194f
monanxiao <monanxiao@qq.com>
2021-09-17 15:11:50 +0000

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 
 });
