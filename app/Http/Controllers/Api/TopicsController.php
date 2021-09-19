669d54133f515bfad5b557e0c20efa4e7e0cb70e
monanxiao <monanxiao@qq.com>
2021-09-19 14:29:13 +0000

    话题列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,9 +6,58 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Http\Resources\TopicResource;
 use App\Http\Requests\Api\TopicRequest;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
+
+    // 话题列表
+    public function index(Request $request, Topic $topic)
+    {
+
+        $topics = QueryBuilder::for(Topic::class) // 模型类
+                        ->allowedIncludes('user', 'category') // 预加载的方法 传入某个，返回某个数据，默认不返回
+                        ->allowedFilters([ // 传入被搜索的数据
+                            'title', // 标题
+                            AllowedFilter::exact('category_id'), // 分类ID
+                            AllowedFilter::scope('withOrder')->default('recentReplied'),// 排序规则
+                        ])
+                        ->paginate();
+
+        // $query = $topic->query();// 执行语句
+        // // 判断查询类型
+        // if ($categoryId = $request->category_id) {
+        //     // id查询
+        //     $query->where('category_id', $categoryId);
+        // }
+
+        // $topics = $query->with('user', 'category') // 预加载关联方法
+        //                 ->withOrder($request->order) // 排序规则
+        //                 ->paginate();// 分页
+        // 返回资源集合
+        return TopicResource::collection($topics);
+    }
+
+    // 某个用户发布的话题
+    public function userIndex(Request $request, User $user)
+    {
+        // 获取用户发布的话题
+        $query = $user->topics()->getQuery();
+
+        $topics = QueryBuilder::for($query)
+            ->allowedIncludes('user', 'category')// 预加载的方法 传入某个，返回某个数据，默认不返回
+            ->allowedFilters([// 传入被搜索的数据
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])
+            ->paginate();
+
+        return TopicResource::collection($topics);
+    }
+
     // 话题发布
     public function store(TopicRequest $request, Topic $topic)
     {
--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -29,6 +29,8 @@ class TopicResource extends JsonResource
             'slug' => $this->slug,
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
+            'user' => new UserResource($this->whenLoaded('user')),// 加载用户模型
+            'category' => new CategoryResource($this->whenLoaded('category')),// 加载类模型
         ];
     }
 }
